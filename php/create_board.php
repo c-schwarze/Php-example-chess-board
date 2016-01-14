f3c43bf7f03d490aae10859f66c177f2e653b3d4
c-schwarze <ces55739@bethel.edu>
2016-01-14 13:59:21 -0600

    created logic for classes

--- php/create_board.php
+++ php/create_board.php
@@ -1,1 +1,16 @@
+<?php
+
+$color_array = array();
+$color = 'white';
+
+for( $i = 0; $i < 8; $i++ ){
+	$color_array[$i] = array();
+	for( $j = 0; $j < 8; $j++ ){
+		array_push($color_array[$i], $color);
+		if( $color == 'white')
+			$color = 'black';
+		else
+			$color = 'white';
+	}
+}
 
