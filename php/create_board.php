488a0d4fcbb9cbfa686be6da1a759171fe76b0ff
c-schwarze <ces55739@bethel.edu>
2016-01-14 14:08:16 -0600

    hook-up twig and add a table

--- php/create_board.php
+++ php/create_board.php
@@ -1,4 +1,5 @@
 <?php
+require_once '/Users/ces55739/Desktop/Php-example-chess-board/vendor/autoload.php';
 
 $color_array = array();
 $color = 'white';
@@ -13,4 +14,12 @@ for( $i = 0; $i < 8; $i++ ){
 			$color = 'white';
 	}
 }
+Twig_Autoloader::register();
 
+$loader = new Twig_Loader_Filesystem('/Users/ces55739/Desktop/Php-example-chess-board/twig/');
+$twig = new Twig_Environment($loader);
+$html = $twig->render('board.html', array(
+	'color_array' => $color_array
+	));
+
+echo $html;
