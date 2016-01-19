99233ae8f8732ac5d69286fd3e8078566d56662e
c-schwarze <ces55739@bethel.edu>
2016-01-19 09:51:31 -0600

    moved files into functions. simplified logic

--- php/create_board.php
+++ php/create_board.php
@@ -1,29 +1,29 @@
 <?php
-require_once '/Users/ces55739/Desktop/Php-example-chess-board/vendor/autoload.php';
 
-$color_array = array();
-$color = 0;
+include_once 'general_functions.php';
+createTwigEnv();
+echo createChessBoard();
 
-for( $i = 0; $i < 8; $i++ ){
-	$color_array[$i] = array();
-	for( $j = 0; $j < 8; $j++ ){
-		if( $color == 0){
-			array_push($color_array[$i], 'white');
-			$color = 1;
-		} else{
-			array_push($color_array[$i], 'black');
-			$color = 0;
+function createChessBoard(){
+	$color_array = array();
+	$color = 1;
+
+	for( $row = 0; $row < 8; $row++ ){
+		$color_array[$row] = array();
+		for( $col = 0; $col < 8; $col++ ){
+			// push each color onto each column of the row
+			array_push($color_array[$row], $color);
+			$color = switchColor($color);
 		}
-
-			
+		// switch after every row, since you want each row to alternate colors
+		$color = switchColor($color);
 	}
-	$color = ($color + 1)%2;
+
+	$html = twig('/twig', 'board.html', array('color_array' => $color_array) );
+
+	return $html;
 }
 
-$loader = new Twig_Loader_Filesystem('/Users/ces55739/Desktop/Php-example-chess-board/twig/');
-$twig = new Twig_Environment($loader);
-$html = $twig->render('board.html', array(
-	'color_array' => $color_array
-	));
-
-echo $html;
+function switchColor( $color ){
+	return ($color + 1)%2;
+}
